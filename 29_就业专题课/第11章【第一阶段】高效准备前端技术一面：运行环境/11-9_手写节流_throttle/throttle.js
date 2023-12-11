c397ec086f3c4997d8cc6c33b793222d443acb00
codersjj <shanewestlife@outlook.com>
2023-12-11 23:27:29 +0800

    feat(第11章-运行环境/11-9_手写节流_throttle): 封装 throttle 函数

--- 29_就业专题课/第11章【第一阶段】高效准备前端技术一面：运行环境/11-9_手写节流_throttle/throttle.js
+++ 29_就业专题课/第11章【第一阶段】高效准备前端技术一面：运行环境/11-9_手写节流_throttle/throttle.js
@@ -1,10 +1,15 @@
 const box = document.getElementById('drag')
-let timer = null
-box.addEventListener('drag', function(e) {
-  if (timer) return
-  timer = setTimeout(() => {
-    console.log(e.offsetX, e.offsetY)
-    timer = null
-  }, 100)
-})
+box.addEventListener('drag', throttle(function(e) {
+  console.log(e.offsetX, e.offsetY)
+}, 200))
 
+function throttle(fn, wait = 100) {
+  let timer = null
+  return function() {
+    if (timer) return
+    timer = setTimeout(() => {
+      fn.apply(this, arguments)
+      timer = null
+    }, wait)
+  }
+}
